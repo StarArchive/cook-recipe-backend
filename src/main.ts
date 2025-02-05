84b896e777cc09c476759486e6dabbbc6fa191f6
StarHeartHunt <starheart233@gmail.com>
2025-02-05 15:36:18 +0800

    chore: init project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Cook recipe backend")
+    .setDescription("Cook recipe backend API portal")
+    .setVersion("1.0")
+    .addTag("recipes")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
