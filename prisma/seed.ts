d576a3a83fda29d63163c16d834f3313e0cbc541
StarHeartHunt <starheart233@gmail.com>
2025-02-09 13:11:53 +0800

    feat: init prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,23 @@
-// prisma/seed.ts
-
 import { PrismaClient } from "@prisma/client";
 
-// initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy articles
-  const post1 = await prisma.article.upsert({
-    where: { title: "Prisma Adds Support for MongoDB" },
+  const alice = await prisma.ingredient.upsert({
+    where: { id: 1 },
     update: {},
     create: {
-      title: "Prisma Adds Support for MongoDB",
-      body: "Support for MongoDB has been one of the most requested features since the initial release of...",
-      description:
-        "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
-      published: false,
+      name: "土豆",
     },
   });
-
-  const post2 = await prisma.article.upsert({
-    where: { title: "What's new in Prisma? (Q1/22)" },
-    update: {},
-    create: {
-      title: "What's new in Prisma? (Q1/22)",
-      body: "Our engineers have been working hard, issuing new releases with many improvements...",
-      description:
-        "Learn about everything in the Prisma ecosystem and community from January to March 2022.",
-      published: true,
-    },
-  });
-
-  console.log({ post1, post2 });
+  console.log({ alice });
 }
-
-// execute the main function
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    // close Prisma Client at the end
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
