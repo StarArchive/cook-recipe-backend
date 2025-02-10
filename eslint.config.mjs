8c1b5057ee7429607193572a63288e7a5f43f1c0
StarHeartHunt <starheart233@gmail.com>
2025-02-10 14:37:21 +0800

    chore(linter): add eslint import to sort import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,40 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import pluginImport from "eslint-plugin-import-x";
 import configPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export const imports = {
+  plugins: {
+    import: pluginImport,
+  },
+  rules: {
+    "import/no-named-as-default": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          { group: "builtin", pattern: "vue", position: "before" },
+          { group: "internal", pattern: "{{@,~}/,#}**" },
+        ],
+        pathGroupsExcludedImportTypes: [],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+  },
+};
 
 const typescriptEslintConfig = tseslint.config(
   {
@@ -19,4 +53,4 @@ const typescriptEslintConfig = tseslint.config(
   },
 );
 
-export default [...typescriptEslintConfig, configPrettierRecommended];
+export default [...typescriptEslintConfig, configPrettierRecommended, imports];
