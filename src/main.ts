6286f1ab20501e67d2a60c76e8b32a3ffbd1984e
StarHeartHunt <starheart233@gmail.com>
2025-02-11 11:28:19 +0800

    refactor(app): use config provider and global validate

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
@@ -5,16 +7,26 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get("PORT");
 
   const config = new DocumentBuilder()
     .setTitle("Cook recipe backend")
     .setDescription("Cook recipe backend API portal")
     .setVersion("0.1")
+    .addBearerAuth()
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(port);
 }
 bootstrap();
